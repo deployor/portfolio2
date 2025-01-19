9b526643654beb88713f626c3e53fec7b9ce5995
Tom (Whity) <129990841+deployor@users.noreply.github.com>
2025-01-20 00:44:09 +0100

    Tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="3f8f134c-c1b0-4ca7-b581-78e365290810"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp;
