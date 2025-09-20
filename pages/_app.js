a1ba7e65b039976fa613f7782bed4710d9332b7e
Deployor <129990841+deployor@users.noreply.github.com>
2025-09-20 20:33:17 +0200

    EE

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,18 @@
 import '../styles/globals.css';
-import Head from 'next/head'
+
+function ComingSoon() {
+  return (
+    <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#0b0b0f', color: '#ffffff', textAlign: 'center', padding: '2rem' }}>
+      <div>
+        <h1 style={{ fontSize: '2rem', fontWeight: 700, marginBottom: '0.5rem' }}>V3 Coming Soon</h1>
+        <p style={{ opacity: 0.8 }}>Site WIP</p>
+      </div>
+    </div>
+  );
+}
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <script
-          defer
-          src="https://cloud.umami.is/script.js"
-          data-website-id="3f8f134c-c1b0-4ca7-b581-78e365290810"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <ComingSoon />;
 }
 
 export default MyApp;
